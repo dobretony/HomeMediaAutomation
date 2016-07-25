70f453a360c07f3353bac80ba60ed4b96bcae1ad
Dobre Antonel <dobre.tony@gmail.com>
2016-07-25 18:11:47 +0300

    Adding first implementation of cmake
    Signed-off-by: Dobre Antonel <dobre.tony@gmail.com>

---
+++ server/HomeAutomationServerConfig.h
@@ -0,0 +1,3 @@
+// the configured options and settings for HomeAutomationServer
+#define HomeAutomationServer_VERSION_MAJOR 0
+#define HomeAutomationServer_VERSION_MINOR 1
---
+++ server/src/main.c
@@ -0,0 +1,28 @@
+/**
+ * Home Media Automation Server is a lightweight command enabler for a home environment.
+ *
+ * It was initially designed to work on a Raspberry PI, so that it can act as a central hub
+ * for home automation.
+ *
+ * The main features are:
+ *
+ * - Enabling start/stop commands for PCs and other devices
+ * - Playback of media stored on Network Attach Storage to either TV or Bluetooth Audio
+ * - Downloading of media files automatically
+ *
+ * /
+
+#include<iostream>
+#include<stdio.h>
+
+
+
+int main(int argc, char** argv){
+
+
+    
+
+
+
+    return 0;
+}
